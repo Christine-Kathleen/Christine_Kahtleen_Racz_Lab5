3702a9cf91f84e80e45a1ab1c2279b7fdcc2e0d0
CHRISTINE-KATHLEEN RÁCZ <christine.racz@stud.ubbcluj.ro>
2020-11-04 21:29:52 +0200

    Add project files.

---
+++ MyPhoneNumbersTableAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Christine_Kahtleen_Racz_Lab5.PhoneNumbersDataSetTableAdapters
+{
+    public partial class PhoneNumbersTableAdapter
+    {
+        public PhoneNumbersTableAdapter(String connectionString)
+        {
+            this.ClearBeforeFill = true;
+            this.Connection = new
+           System.Data.SqlClient.SqlConnection(connectionString);
+        }
+    }
+    class MyPhoneNumbersTableAdapter
+    {
+    }
+}
